ec98a68c1d97e44abc2a1c421ed5e6ac9511d13a
Lorenzo Druziani Andreeta <lorenzo.dz@hotmail.com>
2024-06-01 18:44:02 -0300

    feat: Update Android app label and add SQLite connection lifecycle management

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
 
+import 'core/database/sqlite_adm_connection.dart';
 import 'modules/splash/splash_page.dart';
 
-class AppWidget extends StatelessWidget {
+class AppWidget extends StatefulWidget {
   const AppWidget({Key? key}) : super(key: key);
 
   @override
+  _AppWidgetState createState() => _AppWidgetState();
+}
+
+class _AppWidgetState extends State<AppWidget> {
+  final sqliteAdmConnection = SqliteAdmConnection();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance?.addObserver(
+      sqliteAdmConnection,
+    );
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance?.removeObserver(
+      sqliteAdmConnection,
+    );
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Todo List Provider',
       home: SplashPage(),
     );
