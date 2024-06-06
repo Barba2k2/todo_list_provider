b704e72d8df9e417b86dfe653d7ee7f528dc2b02
Lorenzo Druziani Andreeta <lorenzo.dz@hotmail.com>
2024-06-05 23:37:23 -0300

    feat: Add home drawer to home page

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/home_drawer.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -14,73 +16,8 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Display Large',
-              style: Theme.of(context).textTheme.displayLarge,
-            ),
-            Text(
-              'Display Medium',
-              style: Theme.of(context).textTheme.displayMedium,
-            ),
-            Text(
-              'Display Small',
-              style: Theme.of(context).textTheme.displaySmall,
-            ),
-            Text(
-              'Headline Large',
-              style: Theme.of(context).textTheme.headlineLarge,
-            ),
-            Text(
-              'Headline Medium',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Text(
-              'Headline Small',
-              style: Theme.of(context).textTheme.headlineSmall,
-            ),
-            Text(
-              'Title Large',
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-            Text(
-              'Title Medium',
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-            Text(
-              'Title Small',
-              style: Theme.of(context).textTheme.titleSmall,
-            ),
-            Text(
-              'Body Large',
-              style: Theme.of(context).textTheme.bodyLarge,
-            ),
-            Text(
-              'Body Medium',
-              style: Theme.of(context).textTheme.bodyMedium,
-            ),
-            Text(
-              'Body Small',
-              style: Theme.of(context).textTheme.bodySmall,
-            ),
-            Text(
-              'Label Large',
-              style: Theme.of(context).textTheme.labelLarge,
-            ),
-            Text(
-              'Label Medium',
-              style: Theme.of(context).textTheme.labelMedium,
-            ),
-            Text(
-              'Label Small',
-              style: Theme.of(context).textTheme.labelSmall,
-            ),
-          ],
-        ),
-      ),
+      drawer: HomeDrawer(),
+      body: Container(),
     );
   }
 }
---
+++ lib/app/modules/home/widgets/home_drawer.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../core/auth/auth_provider.dart';
+import '../../../core/ui/messages.dart';
+import '../../../core/ui/theme_extensions.dart';
+import '../../../services/user/user_service.dart';
+
+class HomeDrawer extends StatelessWidget {
+  final nameVN = ValueNotifier<String>('');
+
+  HomeDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: context.primaryColor.withAlpha(70),
+            ),
+            child: Row(
+              children: [
+                Selector<TodoListAuthProvider, String>(
+                  selector: (context, authProvider) {
+                    return authProvider.user?.photoURL ??
+                        'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQM78CdN1ec7XmOaPKLnzLP2CbKNWgEeGNzzzHx_6w-WOBAyhOD4DzKhAypWV57t6_93zvPpoFNyWppUTwNM-qaEHS5ItdqwPWAnwnxPDs';
+                  },
+                  builder: (_, value, __) {
+                    return CircleAvatar(
+                      backgroundImage: NetworkImage(value),
+                      radius: 30,
+                    );
+                  },
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Selector<TodoListAuthProvider, String>(
+                      selector: (context, authProvider) {
+                        return authProvider.user?.displayName ??
+                            'Nao informado';
+                      },
+                      builder: (_, value, __) {
+                        return Text(
+                          value,
+                          style: context.textTheme.titleMedium,
+                        );
+                      },
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          ListTile(
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (_) => AlertDialog(
+                  title: const Text('Alterar nome'),
+                  content: TextField(
+                    onChanged: (value) => nameVN.value = value,
+                    decoration: InputDecoration(
+                      labelText: 'Nome',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide(
+                          width: 1.0,
+                          color: context.primaryColor,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide(
+                          width: 2.0,
+                          color: context.primaryColor,
+                        ),
+                      ),
+                      focusedErrorBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: const BorderSide(
+                          width: 3.0,
+                          color: Colors.red,
+                        ),
+                      ),
+                    ),
+                  ),
+                  actions: [
+                    TextButton(
+                      onPressed: () => Navigator.of(context).pop(),
+                      child: const Text(
+                        'Cancelar',
+                        style: TextStyle(
+                          color: Colors.red,
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () async {
+                        final nameValue = nameVN.value;
+                        if (nameValue.isEmpty) {
+                          Messages.of(context).showError(
+                            'Nome não pode ser vazio',
+                          );
+                        } else {
+                          await context
+                              .read<UserService>()
+                              .updateDisplayName(nameValue);
+                          Navigator.of(context).pop();
+                        }
+                      },
+                      child: const Text('Alterar'),
+                    ),
+                  ],
+                ),
+              );
+            },
+            title: const Text('Alterar nome'),
+          ),
+          ListTile(
+            title: const Text('Sair'),
+            onTap: () => context.read<TodoListAuthProvider>().logout(),
+          )
+        ],
+      ),
+    );
+  }
+}
